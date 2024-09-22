a9c6cb1d4145586b7abfe329a29f245531b31aaf
sam_nodeblack <psam1017@nodeblack.io>
2024-09-22 19:37:48 +0900

    test: UserPoint 단위 테스트 추가
    간단한 테스트들이라서 TDD 없이 그냥 바로 처리했습니다.

--- src/test/java/io/hhplus/tdd/unit/TddApplicationUnitTest.java
+++ src/test/java/io/hhplus/tdd/unit/TddApplicationUnitTest.java
@@ -12,7 +12,6 @@ public abstract class TddApplicationUnitTest {
 
     protected static final BeanContainer BEAN_CONTAINER = new BeanContainer();
 
-    // TODO: 2024-09-22 만약 도메인 메서드가 생긴다면 단위 테스트
     // TODO: 2024-09-22 통합 테스트 진행
     // TODO: 2024-09-21 포인트 정책 추가. 잔고부족 및 최대잔고 등
     // TODO: 2024-09-21 integration package 에서 동시성 제어를 하기 위한 테스트 케이스 작성 후 동시성 제어 기능 구현
